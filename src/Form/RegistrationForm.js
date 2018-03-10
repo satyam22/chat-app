f89805bef9ddace6f16708f5728cea3b54cbb612
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-03-10 11:22:42 +0530

    commit

--- src/Form/RegistrationForm.js
+++ src/Form/RegistrationForm.js
@@ -19,7 +19,21 @@ const residences = [{
             label: 'indore'
         }
         ]
-    }]
+    },
+    {
+        value: 'maharastra',
+        label: 'maharastra',
+        children: [{
+            value: 'Nagpur',
+            label: 'Nagpur'
+        },
+        {
+            value: 'Pune',
+            label: 'Pune'
+        }
+        ]
+    }
+]
 }];
 class RegistrationForm extends Component {
     state = {
@@ -94,72 +108,106 @@ class RegistrationForm extends Component {
         const prefixSelector = getFieldDecorator('prefix', { initialValue: '91' })(
             <Select style={{ width: 70 }}>
                 <Option value="91">+91</Option>
-                <Option value="91">+89</Option>
+                <Option value="89">+89</Option>
             </Select>
         )
         const websiteOptions = autoCompleteResult.map(website => (
             <AutoCompleteOption key={website}>{website}</AutoCompleteOption>))
-            return(
-                <Form onSubmit={this.handleSubmit}>
+        return (
+            <Form onSubmit={this.handleSubmit}>
                 <FormItem {...formItemLayout} label="E-mail">
-                {
-                    getFieldDecorator('email',{
-                        rules:[{
-                            type:'email',message:'The input is not valid E-mail'
-                        },{
-                            required:true,
-                            message:'Please input your E-mail'
-                        }]
-                    })(<Input placeholder="email"/>)
-                }</FormItem>
+                    {
+                        getFieldDecorator('email', {
+                            rules: [{
+                                type: 'email', message: 'The input is not valid E-mail'
+                            }, {
+                                required: true,
+                                message: 'Please input your E-mail'
+                            }]
+                        })(<Input placeholder="email" />)
+                    }</FormItem>
                 <FormItem {...formItemLayout} label="Password">
-                {
-                    getFieldDecorator('password',{
-                        rules:[{
-                            required:true,message:'Please input your password!'
-                        },{
-                            validator:this.validateToNextPassword
-                        }]
-                    })(<Input type="password" />)
-                }</FormItem>
+                    {
+                        getFieldDecorator('password', {
+                            rules: [{
+                                required: true, message: 'Please input your password!'
+                            }, {
+                                validator: this.validateToNextPassword
+                            }]
+                        })(<Input type="password" />)
+                    }</FormItem>
                 <FormItem {...formItemLayout} label="Confirm Password">
-                {
-                    getFieldDecorator('confirm',{
-                        rules:[
-                            {
-                                required:true,message:'Please confirm your password!'
-                            },{
-                                validator:this.compareToFirstPassword
-                            }
-                        ]
-                    })(<Input type="password" onBlur={this.handleConfirmBlur} />)
-                }</FormItem>
+                    {
+                        getFieldDecorator('confirm', {
+                            rules: [
+                                {
+                                    required: true, message: 'Please confirm your password!'
+                                }, {
+                                    validator: this.compareToFirstPassword
+                                }
+                            ]
+                        })(<Input type="password" onBlur={this.handleConfirmBlur} />)
+                    }</FormItem>
                 <FormItem {...formItemLayout}
-                label={(
-                    <span>
-                        Nickname&nbsp;
+                    label={(
+                        <span>
+                            Nickname&nbsp;
                         <Tooltip title="What do you want others to call you?">
-                        <Icon type="question-circle-o" />
-                        </Tooltip>
-                    </span>
-                )}>
-                {getFieldDecorator('nickname',{
-                    rules:[{required:true,message:'Please input your nickname!'}]
-                })(<Input />)
-                }
+                                <Icon type="question-circle-o" />
+                            </Tooltip>
+                        </span>
+                    )}>
+                    {getFieldDecorator('nickname', {
+                        rules: [{ required: true, message: 'Please input your nickname!' }]
+                    })(<Input />)
+                    }
                 </FormItem>
                 <FormItem {...formItemLayout} label="Residence Address">
-                {
-                    getFieldDecorator('residence',{
-                        rules:[{
-                            type:'array',required:true,message:'Please select your residence Address'
-                        }]
-                    })
-                }
+                    {
+                        getFieldDecorator('residence', {
+                            rules: [{
+                                type: 'array', required: true, message: 'Please select your residence Address'
+                            }]
+                        })(<Cascader options={residences} />)
+                    }
                 </FormItem>
-                </Form>
-            )
+                <FormItem {...formItemLayout} label="Phone Number">
+                    {
+                        getFieldDecorator('phone', {
+                            rules: [{ required: true, message: 'Please input your phone number' }]
+                        })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)
+                    }</FormItem>
+                <FormItem {...formItemLayout} label="Website">
+                    {getFieldDecorator('website', {
+                        rules: [{ required: true, message: 'Please input website!' }]
+                    })(<AutoComplete dataSource={websiteOptions} onChange={this.handleWebsiteChange} placeholder="website">
+                        <Input />
+                    </AutoComplete>)
+                    }</FormItem>
+                    <FormItem {...formItemLayout} label="Captcha" extra="We must make sure than you are a human.">
+                    <Row gutter={8}>
+                    <Col span={12}>
+                    {getFieldDecorator('captcha',{
+                        rules:[{required:true,message:'Please input the captcha you got!'},
+                    ]
+                    })(<Input />)
+                    }
+                    </Col>
+                    <Col span={12}>
+                    <Button>Get captcha</Button>
+                    </Col>
+                    </Row>
+                    </FormItem>
+                    <FormItem {...tailFormItemLayout}>
+                    {getFieldDecorator('aggrement',{
+                        valuePropName:'checked',
+                    })(<Checkbox>I have read the <a href="">aggrement</a></Checkbox>)}
+                    </FormItem>
+                    <FormItem {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">Register</Button></FormItem>
+            </Form>
+        )
     }
 }
-const WrappedRegistrationForm=Form.create()(RegistrationForm);
+const WrappedRegistrationForm = Form.create()(RegistrationForm);
 export default WrappedRegistrationForm;
